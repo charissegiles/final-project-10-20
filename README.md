f4a91985fefa0119e95ac930d9db1a5dac09146e
Charisse <charissegiles09@gmail.com>
2020-11-18 21:55:22 -0600

    edited js

--- README.md
+++ README.md
@@ -1,1 +1,107 @@
-#  Oct 2020 Cohort Final Project
+#  Oct 2020 Cohort Final Project\
+## MVP
+
+Create a recipe app that displays pictures of food and allows users to add recipes
+
+- Create an animated carousel of pictures (CSS)
+    - [T: 45 mins, S: C, P: H]
+- Create a carousel of divs that displays the recipes - using arrows to navigate (JS)
+    - [T: 2 hours, S: N, P: H]
+
+
+## POST-MVP
+
+- Create a nav to navigate from homepage to add recipe page
+    - [T: 60 mins, S: N/C, P: H]
+- Create a form that allows users to add recipe
+    - Input: Name of Recipe 
+    - Input: Ingredients 
+    - Input: Instructions
+    - Button: Submit 
+    - [T: 30 mins, S: C, P: H]
+- Format add recipe page
+    - [T: 2+ hours, S: N/X, P: H]
+- Preview the recipe entered
+    - [T: 2+ hours, S: N/X, P: H]
+- Edit and Submit Button
+    - [T: 30 mins, S: C, P: M]
+- Send the submitted info to the recipes carousel in homepage
+    - [T: 2+ hours, S: N/X, P: M]
+- Add functionality to all the buttons
+    - [T: 2+ hours, S: N/X, P: M]
+
+## Pseudocode
+
+- Create an animated carousel of pictures (CSS)
+    
+    Create a div that contains the images
+    <div id="window">
+        <div id="imageslider">
+            <img>
+            <img>
+            <img>
+            <img>
+            <img>
+            //maybe more images
+        </div>
+    </div>
+
+    (CSS)
+    set images to same size
+
+    set window and imageslider same size as images
+
+    set imageslider to display flex
+
+    put animation on imageslider -iteration-count:infinite, 
+    direction: alternate-reverse
+
+    keyframes: from/to: transform: translateX(0 to depends on how many images)
+
+
+    
+- Create a carousel of divs that displays the recipes - using arrows to navigate (JS)
+    
+      Create a div that contains divs that contains the recipes and 2 divs that holds the buttons
+    <div class="buttons">
+        <button onclick="prev()"> < </button>
+    </div>
+    <div id="window2">
+        <div id="recipeslider">
+            <div>recipe</div>
+            <div>recipe</div>
+            <div>recipe</div>
+            <div>recipe</div>
+            //maybe more recipes
+        </div>
+    </div>
+    <div class="buttons">
+        <button onclick="next()"> > </button>
+    </div>
+    
+    CSS
+
+    Put the .buttons, #window 2 side by side
+    set .buttons, #window2 to display: flex 
+
+    set size of #window2 and #recipeslider
+
+    JS
+
+    set currentPosition
+    function next(){
+        set condition so that no blank spaces show 
+            set currentPosition for translate to the left
+            translate #recipeslider 
+    }
+
+    function nprev(){
+        set condition so that no blank spaces show 
+            set currentPosition for translate to the right
+            translate #recipeslider 
+    }
+
+## Mock-Up
+![Mock-Up](./recipeApp.png)
+![Mock-Up](./addRecipePage.png)
+
---
+++ mvp.js
@@ -0,0 +1,102 @@
+function addNewPage() {
+  var section1 = document.getElementById("home");
+  var section2 = document.getElementById("addRecipe");
+  section1.style.display = "none";
+  section2.style.display = "block";
+}
+
+function home() {
+  var section1 = document.getElementById("home");
+  var section2 = document.getElementById("addRecipe");
+  section1.style.display = "block";
+  section2.style.display = "none";
+}
+
+//make the current position dynamic, when user adds new recipe, the new recipe will be added to total index
+
+var lastPosition = -3600; //add to this when new recipe is added
+var currentPosition = 0; //index of first recipe
+function left() {
+  if (currentPosition < 0) {
+    currentPosition = currentPosition + 600;
+    document.getElementById("cardSlider").style.transform =
+      "translateX(" + currentPosition + "px)";
+  }
+}
+
+function right() {
+  if (currentPosition > lastPosition) {
+    currentPosition = currentPosition - 600;
+    document.getElementById("cardSlider").style.transform =
+      "translateX(" + currentPosition + "px)";
+  }
+}
+//-------------------------------------------------------------
+var list = [];
+
+function addRecipe() {
+  list.push({
+    recipeName: document.getElementById("recipeName").value,
+    ingredients: document.getElementById("ingredients").value,
+    instructions: document.getElementById("instructions").value,
+  });
+
+//   var newReceipe = document.createElement("div");
+//   newReceipe.classList.add("container");
+//   newReceipe.innerHTML = ` <div class="card">
+//         <h2>document.getElementById("recipeName").value</h2>
+//         </div>
+//         <div class="card">
+//         <h4>Ingredients:</h4>
+//         </div>
+//         <div class="card">
+//         document.getElementById("ingredients").value
+//         </div>
+//         <div class="card">
+//         <h4>Instructions:</h4>
+//         </div>
+//         <div class="card">
+//         document.getElementById("instructions").value
+//         </div> `;
+
+//   document.getElementById("cardSlider").appendChild(recipeCard);
+  renderRecipeCard()
+  //document.getElementById("cardSlider").appendChild(recipeCard);
+  //lastPosition = lastPosition - 600;
+  
+}
+
+var renderRecipeCard = function () {
+  // var clearList = document.getElementById('cardSlider');
+  // clearList.textContent = '';
+  
+    // create a var for restCard
+    var recipeCard = document.createElement("div");
+    //update innerHTML with the child elements
+    recipeCard.innerHTML = `
+            <div class="container">
+                <div class="card">
+                    <h2>${list[i].recipeName}</h2>
+                </div>
+                <div class="card">
+                    <h4>Ingredients:</h4>
+                </div>
+                <div class="card">
+                    ${list[i].ingredients}
+                </div>
+                <div class="card">
+                    <h4>Instructions:</h4>
+                </div>
+                <div class="card">
+                    ${list[i].instructions}
+                </div> 
+            </div>
+        `;
+    
+  
+};
+var readList = function() {
+    for(i=0; i<list.length; i++){
+        console.log(i)
+    }
+}
